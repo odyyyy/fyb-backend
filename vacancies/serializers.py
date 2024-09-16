cc8dd4fe907be97c40e12519a40b341a93eef054
Ilya <iliya.odyvan4ikys@gmail.com>
2024-09-16 17:40:05 +0300

    refactor: serializer is responsible for selecting and creating specific vacancy type

--- vacancies/serializers.py
+++ vacancies/serializers.py
@@ -1,38 +1,59 @@
 from rest_framework import serializers
 
-from vacancies.models import MusicianVacancy, BandVacancy, OrganizerVacancy
-
-from rest_framework import serializers
 from .models import MusicianVacancy, BandVacancy, OrganizerVacancy
 
 
-class FavoriteVacancySerializer(serializers.Serializer):
+class VacancyWithDateTimeMixin:
+    date = serializers.DateTimeField(write_only=True)
 
+
+class FavoriteVacancySerializer(serializers.Serializer):
 
     def to_representation(self, vacancy_obj):
         return choose_vacancy_serializer(vacancy_obj)
 
 
 class VacanciesBaseSerializer(serializers.Serializer):
+    type = serializers.CharField(write_only=True)
 
     def to_representation(self, vacancy_obj):
         return choose_vacancy_serializer(vacancy_obj)
 
+    def run_validation(self, data):
+        """ Не выполняем валидацию для полей т.к.
+         сериализатор выполняет роль посредника """
+        return data
 
-class MusicianVacancySerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        vacancy_type = validated_data.pop('type', None)
+
+        if vacancy_type == 'musician':
+            serializer = MusicianVacancySerializer(data=validated_data)
+        elif vacancy_type == 'band':
+            serializer = BandVacancySerializer(data=validated_data)
+        elif vacancy_type == 'organizer':
+            serializer = OrganizerVacancySerializer(data=validated_data)
+        else:
+            raise serializers.ValidationError('Unknown vacancy type')
+
+        if serializer.is_valid(raise_exception=True):
+            return serializer.save()
+        raise serializers.ValidationError('Invalid data for the vacancy type')
+
+
+class MusicianVacancySerializer(VacancyWithDateTimeMixin, serializers.ModelSerializer):
     class Meta:
         model = MusicianVacancy
         fields = '__all__'
 
 
-
-class BandVacancySerializer(serializers.ModelSerializer):
+class BandVacancySerializer(VacancyWithDateTimeMixin, serializers.ModelSerializer):
     class Meta:
         model = BandVacancy
         fields = '__all__'
 
 
-class OrganizerVacancySerializer(serializers.ModelSerializer):
+class OrganizerVacancySerializer(VacancyWithDateTimeMixin, serializers.ModelSerializer):
     class Meta:
         model = OrganizerVacancy
         fields = '__all__'
@@ -45,3 +66,4 @@ def choose_vacancy_serializer(vacancy_obj):
         return BandVacancySerializer(vacancy_obj).data
     elif isinstance(vacancy_obj, OrganizerVacancy):
         return OrganizerVacancySerializer(vacancy_obj).data
+    raise serializers.ValidationError('Unknown vacancy object type')
