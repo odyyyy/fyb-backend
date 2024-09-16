96662e9f438b24facf4825b55f58d47a2550e68b
Ilya <iliya.odyvan4ikys@gmail.com>
2024-09-16 17:44:33 +0300

    refactor: remove test func

--- users/tasks.py
+++ users/tasks.py
@@ -6,15 +6,6 @@ from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 
 
-@shared_task
-def send_email_test_task():
-    send_mail(
-        subject='Test email',
-        message='Test email',
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=["iliyaign1231@gmail.com", ]
-    )
-
 @shared_task(bind=True, max_retries=3)
 def send_welcome_email(self, user_email):
     try:
@@ -36,12 +27,7 @@ def send_daily_email_if_user_inactive_for_year():
     if inactive_users.exists():
         send_mail(
             subject='Inactive users',
-            message=f'Your account will be blocked if you will not log in next 3 months',
+            message='Your account will be blocked if you will not log in next 3 months',
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[user.email for user in inactive_users]
         )
-
-
-@shared_task
-def test_schedule_task():
-    print('Its test task!')
