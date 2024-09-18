0781c15f08647825e1e28578b317d9d70d4efe3d
Ilya <iliya.odyvan4ikys@gmail.com>
2024-09-18 21:47:50 +0300

    feat: use custom user model

--- users/models.py
+++ users/models.py
@@ -7,22 +7,9 @@ from users.tasks import send_welcome_email
 
 
 class User(AbstractUser):
-    USER_MUSICIAN_ROLE = (('musician', 'Musician'),
-                          ('band', 'Band'),
-                          ('organizer', 'Organizer'))
-
-    role = models.CharField(max_length=50, choices=USER_MUSICIAN_ROLE, default='musician', verbose_name="Роль")
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
     image = models.ImageField(upload_to='media/photos/users/%Y/%m/%d/', blank=True, null=True, verbose_name="Фото")
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='custom_user_set',
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='custom_user_permissions_set',
-        blank=True
-    )
-    
+
     def save(self, *args, **kwargs):
         send_welcome_email.delay(self.email)
         super().save(*args, **kwargs)
