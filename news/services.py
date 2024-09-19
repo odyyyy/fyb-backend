2a23153a9ee1a11d9405b90402882175f67ef930
Ilya <iliya.odyvan4ikys@gmail.com>
2024-09-20 00:34:08 +0300

    refactor: add type hints

--- news/services.py
+++ news/services.py
@@ -1,18 +1,26 @@
 from datetime import datetime
+from typing import List, Dict, Any
+from xml.dom.minidom import parseString
 
 import requests
-from xml.dom.minidom import parseString, Document
-import json
-from django.utils.text import slugify
 from django.core.cache import cache
+from django.utils.text import slugify
 
 NEWS_RSS_URL = "https://pitchfork.com/feed/feed-news/rss"
 NEWS_CACHE_KEY = 'news'
 
 
-def parse_actual_news() -> None:
-    rss_response = requests.get(NEWS_RSS_URL)
-    news_document = parseString(rss_response.content)
+def get_actual_news() -> list:
+    news_data = cache.get(NEWS_CACHE_KEY)
+    if news_data is not None:
+        return news_data
+    parse_actual_news()
+    return cache.get(NEWS_CACHE_KEY)
+
+
+def parse_actual_news() -> list[dict[str, str]]:
+    rss_response_data = requests.get(NEWS_RSS_URL).content
+    news_document = parseString(rss_response_data)
 
     news_items = news_document.getElementsByTagName("item")
     json_news = []
@@ -35,6 +43,7 @@ def parse_actual_news() -> None:
         })
 
     cache.set(NEWS_CACHE_KEY, json_news, 60 * 60)
+    return json_news
 
 
 def get_news_item(slug: str) -> dict:
@@ -43,11 +52,3 @@ def get_news_item(slug: str) -> dict:
         for item in news_data:
             if item['slug'] == slug:
                 return item
-
-
-def get_actual_news() -> list:
-    news_data = cache.get(NEWS_CACHE_KEY)
-    if news_data is not None:
-        return news_data
-    parse_actual_news()
-    return cache.get(NEWS_CACHE_KEY)
