a0c3f997189b77b256cc82f63dc90bc634eb3736
Ilya <iliya.odyvan4ikys@gmail.com>
2024-09-20 00:29:57 +0300

    feat: add tests to news parse func

--- news/tests.py
+++ news/tests.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
-from news.services import get_news_item
+from news.services import parse_actual_news
 
 
 def get_actual_news_mocked():
@@ -31,8 +31,8 @@ def get_news_item_mocked(slug: str):
         if item['slug'] == slug:
             return item
 
-class NewsTestCase(APITestCase):
 
+class NewsAPITestCase(APITestCase):
 
     @patch('news.views.get_actual_news', get_actual_news_mocked)
     def test_news_list(self):
@@ -52,3 +52,45 @@ class NewsTestCase(APITestCase):
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.data, {'error': 'Новость не найдена'})
 
+
+class NewsServiceTestCase(APITestCase):
+
+    @patch('news.services.requests.get')
+    # @patch('news.services.cache.set')
+    def test_parse_actual_news(self, mock_request_get_data):
+        mock_rss_content = '''<rss
+	xmlns:atom="http://www.w3.org/2005/Atom"
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
+	<channel>
+		<title>RSS: News</title>
+		<description>News content RSS feed</description>
+		<item>
+			<title>Test News</title>
+			<link>https://test.com</link>
+			<guid isPermaLink="false">66ec8440c7576e9238fa659a</guid>
+			<pubDate>Thu, 19 Sep 2024 20:53:34 +0000</pubDate>
+			<media:content/>
+			<description>This is a test news item.</description>
+			<category>News</category>
+			<media:keywords>Film</media:keywords>
+			<dc:creator>Madison Bloom</dc:creator>
+			<dc:publisher>Condé Nast</dc:publisher>
+			<media:thumbnail url="http://example.com/image.jpg" width="3000" height="1500"/>
+		</item>
+	</channel>
+</rss>'''
+
+        mock_request_get_data.return_value.content = mock_rss_content
+
+        result_json_news = parse_actual_news()
+
+        expected_news = [{
+            'title': 'Test News',
+            'slug': 'test-news',
+            'description': 'This is a test news item.',
+            'pubDate': '2024-09-19 20:53:34',
+            'image': 'http://example.com/image.jpg',
+        }]
+
+        self.assertEqual(result_json_news, expected_news)
