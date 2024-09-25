6a62a691070d6bb7d19d1c76b9a820e5271101cc
Ilya <iliya.odyvan4ikys@gmail.com>
2024-09-26 00:59:10 +0300

    feat: tests for user profile view

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.serializers import UserProfileSerializer
+
+
+class UsersViewsTests(APITestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='test_user123', email='test_user_email123@mail.com')
+        self.client.force_login(self.user)
+
+    def test_user_profile_view_retrieve(self):
+        response = self.client.get(reverse('users-profile'))
+        serializer = UserProfileSerializer(self.user)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(serializer.data, response.data)
+
+    def test_user_profile_view_update(self):
+        response = self.client.patch(reverse('users-profile'), {'username': 'test_user_new_name'}, format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['username'], 'test_user_new_name')
+        self.assertEqual(get_user_model().objects.get(id=self.user.id).username, 'test_user_new_name')
+
+    def test_user_profile_view_update_email_not_changed(self):
+        response = self.client.patch(reverse('users-profile'), {'email': 'test_user_new_email@mail.com'}, format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(get_user_model().objects.get(id=self.user.id).email, 'test_user_email123@mail.com')
