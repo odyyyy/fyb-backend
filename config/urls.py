eba31323f1961c29a14b74cc07f364aa6fc11b98
Ilya <iliya.odyvan4ikys@gmail.com>
2024-10-20 15:07:27 +0300

    feat: change api url patterns to api/v1

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,23 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
+from debug_toolbar import urls as debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include, re_path
 
 from users.views import auth
 
+api_patterns = [
+    path('vacancies/', include('vacancies.urls')),
+    path('bands/', include('bands.urls')),
+    path('users/', include('users.urls')),
+    path('news/', include('news.urls')),
+]
+
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api-auth/', include('rest_framework.urls')),
-                  re_path('', include('social_django.urls', namespace='social')),
-                  path('auth/', auth),
-                  path('vacancies/', include('vacancies.urls')),
-                  path('bands/', include('bands.urls')),
-                  path('users/', include('users.urls')),
-                  path('news/', include('news.urls')),
-                  path('djdt/', include('debug_toolbar.urls')),
-              ] + debug_toolbar_urls()
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 
+    path('api/v1/', include((api_patterns, 'api'), namespace='api')),
 
+    re_path('', include('social_django.urls', namespace='social')),
+    path('auth/', auth),
+    path('djdt/', include(debug_toolbar_urls)),
+]
