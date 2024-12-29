744cea4554e3573984a82b0b1237fafd55bc1966
Ilya <iliya.odyvan4ikys@gmail.com>
2024-12-29 17:06:01 +0300

    feat: add email notification task

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -12,20 +12,18 @@ logger = logging.getLogger(__name__)
 @shared_task
 def send_new_vacancy_notification(vacancy_model_name: str):
 
-    print('send_new_vacancy_notification:', vacancy_model_name)
     subscriptions = NotificationSubscription.objects.filter(
         notification_type=vacancy_model_name)
 
     recipient_list = [subscription.user.email for subscription in subscriptions if subscription.user.email]
 
     logger.debug(f"Sending email notification to {len(recipient_list)} users")
-    print(recipient_list)
 
-    send_mail(
+    is_mail_sent = send_mail(
         subject=f'Новое {vacancy_model_name}!',
-        message=f'New {vacancy_model_name} was created',
+        message=f'Новое {vacancy_model_name} было создано',
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=recipient_list,
         fail_silently=False)
 
-    logger.debug("Email notification was sent successfully")
+    logger.debug("Email notification was sent successfully" if is_mail_sent else "Email notification wasn't sent")
